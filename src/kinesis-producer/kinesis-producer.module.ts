e475d37389a1c050f5e599acf4f02756eba23cec
Timo Geissler <geissler@twaice.com>
2020-11-06 17:41:50 +0100

    Add Winston Logging Module

--- src/kinesis-producer/kinesis-producer.module.ts
+++ src/kinesis-producer/kinesis-producer.module.ts
@@ -1,5 +1,6 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
-
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import { BatchKinesisPublisher } from './batch-kinesis-publisher';
 import { Kinesis } from 'aws-sdk';
 import { RetryingBatchKinesisPublisher } from './retrying-batch-kinesis-publisher';
@@ -9,6 +10,20 @@ import { RetryingBatchKinesisPublisher } from './retrying-batch-kinesis-publishe
 export class KinesisProducerModule {
   static forRoot(kinesis: Kinesis): DynamicModule {
     return {
+      imports: [
+        WinstonModule.forRoot({
+          transports: [
+            new winston.transports.Console({
+              level: process.env.LOG_LEVEL || 'info',
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                nestWinstonModuleUtilities.format.nestLike(),
+                winston.format.uncolorize(),
+              ),
+            }),
+          ],
+        }),
+      ],
       module: KinesisProducerModule,
       providers: [
         BatchKinesisPublisher,
